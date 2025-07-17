ff93eeb2eb627b921fcced1f99db2914a7972b42
Jatin Dadwal <jatindadwal144@gmail.com>
2025-07-17 16:17:17 +0530

    Create 17 july assignment.py

---
+++ 17 july assignment.py
@@ -0,0 +1,17 @@
+def j(**fname):
+    print(f"my name is{fname}")
+j(fname="jatiiin")    
+
+def j(*fname):
+    print(f"my name is {fname}")
+j("jatin ","dadwal")
+
+def j(num1,num2):
+    num1=int(input("enter first value"))
+    num2=int(input("enter second value"))
+    return num1+num2
+(j(num1,num2))
+
+def  ja(fname):
+    print("hello "+fname)
+ja("jatin Dadwal")  
